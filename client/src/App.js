ca8d5663f672fc9248697bbe86d8ad6429fbd043
amira-02 <amiraoumaymahayet@gmail.com>
2025-05-16 12:20:47 +0000

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Register from "./pages/register";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Login from "./pages/login";
+import Cards from "./pages/cards";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Cards />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+// export default function App(){
+// return <div>aslema</div>;
+// }
